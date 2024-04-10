a5177680c2addf5dd1569a6acc794eb39428185f
JuanCeja <jmc347ls2@yahoo.com>
2024-04-09 18:28:47 -0700

    78. create a user input with two way binding for our state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
 
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol }) => {
 
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
     const handleEditClick = () => {
-        setIsEditing(prevState => !prevState);
+        setIsEditing(editing => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    const handleChange = (event) => {
+        setPlayerName(event.target.value);
+    }
 
-    if (isEditing) playerName = <input type="text" placeholder={name} required/>;
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
 
