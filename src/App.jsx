2285511cd33d8e56bc39dc8eb2a79d1306c90a4a
JuanCeja <jmc347ls2@yahoo.com>
2024-04-17 21:39:27 -0700

    92. fix bug that wasnt letting game restart. gameboard was not properly immutable

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -58,6 +58,10 @@ function App() {
     });
   }
 
+  const handleRestart = () => {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -71,7 +75,7 @@ function App() {
             symbol="O"
             isActive={activePlayer === "O"} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard
           onSelectSquare={handleSelectSquare}
           board={gameBoard} />
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const GameOver = ({ winner }) => {
+const GameOver = ({ winner, onRestart }) => {
     return (
         <div id='game-over'>
             <h2>Game Over!</h2>
@@ -8,7 +8,7 @@ const GameOver = ({ winner }) => {
                 winner ? <p>{winner} has won!</p>
                     : <p>It's have a draw!</p>
             }
-            <p><button>Rematch!</button></p>
+            <p><button onClick={onRestart}>Rematch!</button></p>
         </div>
     )
 }
